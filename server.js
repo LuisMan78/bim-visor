be805c69b3706805b2c305e13cf2a17d78cf5796
LuisMan78 <elpatro009@gmail.com>
2026-04-24 22:36:18 -0500

    fix express route

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 
 app.use(express.static(join(__dirname, 'dist')))
 
-app.get('*', (req, res) => {
+app.get('/{*path}', (req, res) => {
   res.sendFile(join(__dirname, 'dist', 'index.html'))
 })
 
