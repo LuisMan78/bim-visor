b2c33f5f2d252ecd2bd01cffc5611bd8e5022ac4
LuisMan78 <elpatro009@gmail.com>
2026-04-24 22:00:11 -0500

    fix puerto Render

--- src/vite.config.js
+++ src/vite.config.js
@@ -2,14 +2,19 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
+  preview: {
+    host: '0.0.0.0',
+    port: 10000,
+    allowedHosts: 'all'
+  },
+  server: {
+    port: 5173
+  },
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
       }
     }
-  },
-  server: {
-    port: 5173
   }
 })
